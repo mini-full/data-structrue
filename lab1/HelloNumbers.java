0579717a767ba56e253ddea5b14c6e76df0d1750
Slashway <lahn45@foxmail.com>
2022-07-15 23:35:37 +0800

    completed 1st part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        // cs61b lab1
+        // print out the numbers 0 through 9
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
