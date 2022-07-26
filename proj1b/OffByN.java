88b5cef1393d72e218a57e75a9f2775ba00d3929
Mini-Full <Lhan45@foxmail.com>
2022-07-26 19:11:19 +0800

    completed OffByN

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,3 +1,14 @@
-public class OffByN {
+public class OffByN implements CharacterComparator {
     
+    private int N = 0;
+
+    public OffByN(int n) {
+        N = n;
+    }
+
+    public boolean equalChars(char x, char y) {
+        int diff = x - y;
+        return diff == N || diff == -N;
+    }
+
 }
